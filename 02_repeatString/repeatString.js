3446d1f8358b308780d56da9e1210f7ae15e4f72
mariamz296 <147218801+mariamz296@users.noreply.github.com>
2024-09-21 03:45:52 -0400

    completed repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    if (num < 0)
+        return "ERROR";
+    
+    let string = "";
+    let i;
+    for (i=0; i<num; i++) {
+        string = string + word;
+    }
 
+    return string;
 };
 
 // Do not edit below this line
