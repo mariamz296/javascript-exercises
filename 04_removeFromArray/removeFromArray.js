4abf8147b1083be424fd2d26aecd0d4f9e15af2c
mariamz296 <147218801+mariamz296@users.noreply.github.com>
2024-09-23 02:45:37 -0400

    completed removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array1, ...args) {
+    const newArray = [];
+    
+    array1.forEach(element => {
+        if (!args.includes(element)) {
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
