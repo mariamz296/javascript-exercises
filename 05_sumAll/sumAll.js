85696d9f8979a5b4688706d38d1a85c13a96474a
mariamz296 <147218801+mariamz296@users.noreply.github.com>
2024-09-23 02:57:46 -0400

    completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let i, sum = 0;
+    let temp;
+    if (num1 > num2) {
+        temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
 
+    if (num1 < 0 || num2 < 0)
+        return "ERROR";
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2))
+        return "ERROR";
+
+    for (i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
