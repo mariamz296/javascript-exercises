ad763e9fdfc425cec45e0741c1e48e438759b076
mariamz296 <147218801+mariamz296@users.noreply.github.com>
2024-12-27 20:26:57 -0500

    completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentValue) => (total + currentValue), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentValue) => (total * currentValue), 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0)
+    return 1;
+
+  let i, product = 1;
+  for (i=1; i<=num; i++) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
