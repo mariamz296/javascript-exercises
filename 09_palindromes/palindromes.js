21edb27bd399971453b2723b6aba01927525ac95
mariamz296 <147218801+mariamz296@users.noreply.github.com>
2025-01-14 11:15:18 -0500

    completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    function hasCharacter(char) {
+        return alphabet.includes(char); 
+    }
+
+    const newString = string.toLowerCase().split('').filter(hasCharacter).join('');
+
+    const reverseString = newString.split('').reverse().join('');
+
+    return newString === reverseString;
 };
 
 // Do not edit below this line
